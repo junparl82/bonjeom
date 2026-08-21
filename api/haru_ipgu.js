2cda13aa6adcde5147769dab484b6109f65ee5c9
junparl82 <vilieve82@gmail.com>
2026-08-21 17:35:00 +0900

    haru ipgu handler p0-p8 door+sign

--- api/haru_ipgu.js
+++ api/haru_ipgu.js
@@ -6,4 +6,5 @@ import p4 from '../lib/h_ipgu_4.js'
 import p5 from '../lib/h_ipgu_5.js'
 import p6 from '../lib/h_ipgu_6.js'
 import p7 from '../lib/h_ipgu_7.js'
-export default function handler(req,res){const b=Buffer.from(p0+p1+p2+p3+p4+p5+p6+p7,'base64');res.setHeader('Content-Type','image/jpeg');res.setHeader('Cache-Control','public,max-age=60');res.status(200).send(b)}
+import p8 from '../lib/h_ipgu_8.js'
+export default function handler(req,res){const b=Buffer.from(p0+p1+p2+p3+p4+p5+p6+p7+p8,'base64');res.setHeader('Content-Type','image/jpeg');res.setHeader('Cache-Control','public,max-age=60');res.status(200).send(b)}
